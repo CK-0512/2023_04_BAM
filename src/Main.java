13837de0848cca077011c6b8028bdbcbe8fe8eb1
basdzxqwe <ock6166@naver.com>
2023-04-13 19:40:52 +0900

    git 업로드 시작

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("====프로그램 시작====");
+		
+		Scanner scn = new Scanner(System.in);
+		
+//		String cmd = scn.nextLine();
+//		int cmd = scn.nextInt();
+		String cmd = scn.next();
+		
+		System.out.println(cmd);
+
+		System.out.println("====프로그램 끝====");
+	}
+}
