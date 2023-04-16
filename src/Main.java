f64b3b2a6b8b1591db4e7a141555286d2b905c16
basdzxqwe <ock6166@naver.com>
2023-04-16 23:41:37 +0900

    article detail 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,12 @@ public class Main {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 			
+			if (cmd.equals("exit")) {
+				break;
+			}
+			
 			if (cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
 				lastArticleId = id;
 				System.out.printf("제목 : ");
@@ -34,14 +39,37 @@ public class Main {
 					System.out.println("존재하는 게시물이 없습니다.");
 					continue;
 				} 
-					
+				System.out.println("== 게시물 목록 ==");	
 				System.out.println("	번호	|	제목	");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 					System.out.printf("%d	|	%s	\n", article.id, article.title);
 				}
-			} else if (cmd.equals("exit")) {
-				break;
+			} else if (cmd.startsWith("article detail ")) {
+				
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+				
+				Article foundArticle = null;
+				
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					} 
+				}
+				
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+					
+					System.out.println("== 게시물 상세보기 ==");
+					System.out.printf("번호 : %d\n", foundArticle.id);
+					System.out.printf("제목 : %d\n", foundArticle.title);
+					System.out.printf("내용 : %d\n", foundArticle.body);
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다.");
 			}
 		}
 		
