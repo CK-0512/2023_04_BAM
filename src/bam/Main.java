9dd2cdb9399efdaf491b54b44de02c947f7e99a6
basdzxqwe <ock6166@naver.com>
2023-04-17 16:40:51 +0900

    regDate기능 추가

--- src/bam/Main.java
+++ src/bam/Main.java
@@ -1,6 +1,9 @@
+package bam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import bam.util.Util;
 
 public class Main {
 	public static void main(String[] args) {
@@ -23,12 +26,13 @@ public class Main {
 				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
 				lastArticleId = id;
+				String regDate = Util.getDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				
 				articles.add(article);
 				
@@ -40,10 +44,10 @@ public class Main {
 					continue;
 				} 
 				System.out.println("== 게시물 목록 ==");	
-				System.out.println("	번호	|	제목	");
+				System.out.println("	번호	|	제목	|	작성일	");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d	|	%s	\n", article.id, article.title);
+					System.out.printf("%d	|	%s	|%s	\n", article.id, article.title, article.regDate);
 				}
 			} else if (cmd.startsWith("article detail ")) {
 				
@@ -66,6 +70,7 @@ public class Main {
 					
 					System.out.println("== 게시물 상세보기 ==");
 					System.out.printf("번호 : %d\n", foundArticle.id);
+					System.out.printf("작성일 : %s\n", foundArticle.regDate);
 					System.out.printf("제목 : %d\n", foundArticle.title);
 					System.out.printf("내용 : %d\n", foundArticle.body);
 			} else {
@@ -81,12 +86,15 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 	
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
 }
+
